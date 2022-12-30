4aae1633abf27790bb36e5c01d72f6e13b311ecb
Changhogod <younch8342@naver.com>
2022-12-31 01:40:13 +0900

    Stream Example

---
+++ src/Greedy/StreamExample.java
@@ -0,0 +1,23 @@
+package Greedy;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamExample {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("Java", "Scala", "Groovy","Go","Python");
+         list.stream()
+                .sorted().forEach(s-> System.out.println(s));
+
+       List<String> sList =  list.stream().filter(s->s.startsWith("G")).collect(Collectors.toList());
+        System.out.println(sList);
+
+        boolean anyMatch = list.stream().anyMatch(language->language.contains("P"));
+        System.out.println(anyMatch);
+
+
+    }
+
+}
