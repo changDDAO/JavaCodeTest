b28a284e3fc5e660b09372752c2155699dd8aabf
Changhogod <younch8342@naver.com>
2023-01-02 21:56:31 +0900

    AdjacencyMatrix & AdjacencyList

---
+++ src/DFS_BFS/Node.java
@@ -0,0 +1,15 @@
+package DFS_BFS;
+
+public class Node {
+    private int index;
+    private int distance;
+
+    public Node(int index, int distance) {
+        this.index = index;
+        this.distance = distance;
+    }
+
+    public void show() {
+        System.out.print("(" + this.index + "," + this.distance + ") ");
+    }
+}
