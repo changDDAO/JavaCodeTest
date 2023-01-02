a8cd322da86c378efb83d16a952ca06d21a73cb4
Changhogod <younch8342@naver.com>
2023-01-03 00:46:52 +0900

    define DFS & BFS

--- src/DFS_BFS/BFS.java
+++ src/DFS_BFS/BFS.java
@@ -1,26 +1,35 @@
 package DFS_BFS;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class DFS {
+public class BFS {
     public static boolean []visited = new boolean[9];
     public static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    public static void dfs(int x){
-        visited[x]=true;
-        System.out.print(x+" ");
-        for(int i =0;i<graph.get(x).size();i++){
-            int adjacencyValue = graph.get(x).get(i);
-            if(!visited[adjacencyValue])//만약 인접 노드를 방문하지 않았다면
-                dfs(adjacencyValue);
-        }
 
+    public static void bfs(int start){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start]=true;
+        while(!queue.isEmpty()){
+            int x = queue.poll();
+            System.out.print(x+" ");
+            //뽑아낸 노드와 연결된 노드를 큐에 삽입
+            for(int i =0;i<graph.get(x).size();i++){
+                int adjacencyNode =graph.get(x).get(i);
+                if(!visited[adjacencyNode]) {
+                    queue.offer(adjacencyNode);
+                    visited[adjacencyNode]=true;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
-        for(int i =0; i<9;i++){
-            graph.add(new ArrayList<>());
+        for(int i=0;i<9;i++){
+            graph.add(new ArrayList<Integer>());
         }
-
         //1과 2 3 8
         graph.get(1).add(2);
         graph.get(1).add(3);
@@ -53,10 +62,6 @@ public class DFS {
         //8과 1 7
         graph.get(8).add(1);
         graph.get(8).add(7);
-
-        dfs(1);
-
-
-
+        bfs(1);
     }
 }
