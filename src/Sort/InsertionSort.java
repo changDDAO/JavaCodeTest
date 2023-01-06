f78dc4f6c8ebc26ac246465e319c0dc79228cda3
Changhogod <younch8342@naver.com>
2023-01-06 16:57:04 +0900

    Insertion Sort implementation

--- src/Sort/InsertionSort.java
+++ src/Sort/InsertionSort.java
@@ -6,7 +6,7 @@ public class InsertionSort {
         int[] arr = {1,2,7,3,4,5,9,8,6,13};
 //Insertion Sort
         for(int i=1;i<n;i++){
-            //현재 인덱스 기준으로 왼쪽 인덱스의 값과 비교
+            //현재 인덱스 기준으로 왼쪽 인덱스의 값과 비교(1 index까지만)
             for(int j=i;j>0;j--){
                 if(arr[j]<arr[j-1]){//현재 인덱스값이 정렬된 왼쪽 인덱스값과 비교 시 작다면 Insertion
                     int temp = arr[j];
