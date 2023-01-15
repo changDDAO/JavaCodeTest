01ca29b3957912c2237b7f6df719aaaefb8a4ece
Changhogod <younch8342@naver.com>
2023-01-16 01:23:55 +0900

    solve greedy problem Q5

---
+++ src/GreedyProblem/Q5.java
@@ -0,0 +1,56 @@
+package GreedyProblem;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Ball implements Comparable<Ball>{
+    private final int number;
+    private final int weight;
+
+    public Ball(int number, int weight) {
+        this.number = number;
+        this.weight = weight;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public int compareTo(Ball o) {
+        if(this.weight<o.weight)
+            return -1;
+        else return 1;
+    }
+}
+
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //볼링공 번호와 최대무게 입력받기
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        ArrayList<Ball> ballList = new ArrayList<>();
+        for(int i=0;i<N;i++){
+            ballList.add(new Ball(i+1,sc.nextInt()));
+        }
+        Collections.sort(ballList);
+        //이러면 무게별로 정렬이 돼있음(오름차순)/
+        int answer =0;
+        for(int i=0;i<ballList.size();i++){
+            Ball ballA = ballList.get(i);
+            for(int j=i+1;j<ballList.size();j++){
+                Ball ballB = ballList.get(j);
+                if(ballA.getNumber()!=ballB.getNumber()&&ballA.getWeight()!=ballB.getWeight())
+                    answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+
+}
