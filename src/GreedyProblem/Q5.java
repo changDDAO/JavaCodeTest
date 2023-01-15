02def891e47ccad86d47bbef0e87a1b97743e172
Changhogod <younch8342@naver.com>
2023-01-16 01:27:23 +0900

    solve greedy problem Q5

--- src/GreedyProblem/Q5.java
+++ src/GreedyProblem/Q5.java
@@ -40,7 +40,9 @@ public class Q5 {
             ballList.add(new Ball(i+1,sc.nextInt()));
         }
         Collections.sort(ballList);
-        //이러면 무게별로 정렬이 돼있음(오름차순)/
+        //이러면 무게별로 정렬이 돼있음(오름차순)
+        //근데 굳이 정렬한다고해서 빨라질껀없는듯
+        //쉽게 풀수 있지만 가능한 많은 것들을 쓰면서 풀어보고싶었음!
         int answer =0;
         for(int i=0;i<ballList.size();i++){
             Ball ballA = ballList.get(i);
