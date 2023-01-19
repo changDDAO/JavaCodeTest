085eee74e536f38b4f42a8a9efa474a63d4e4384
Changhogod <younch8342@naver.com>
2023-01-19 19:41:25 +0900

    implementation Q10

--- src/ImplementationProblem/Q10.java
+++ src/ImplementationProblem/Q10.java
@@ -1,36 +1,28 @@
 package ImplementationProblem;
+class Q10 {
+    public int solution(String s) {
+        int answer = 0;
 
-public class Q10 {
-    public int solution(String s){
-        int answer = s.length();
-        //1개 단위부터 반까지 늘려가기
-        for(int charCnt=1; charCnt<s.length()/2+1;charCnt++){
-            String compressedString ="";
-            String prev = s.substring(0,charCnt);//앞에서부터 문자 갯수만큼 추출
-            int cnt =1;
-            //문자 갯수만큼 증가시키며 이전 문자열과 비교
-            for(int i=charCnt; i<s.length();i+=charCnt){
-                //이전 문자와 동일하다면 압축횟수 증가
-                String temp = "";
-                for(int j=i;j<j+charCnt;j++){
-                    if(j<s.length()) temp+=s.charAt(j);
-                }
-                if(prev.equals(temp))//이전 문자열과 동일하다면 cnt 증가
-                    cnt++;
-                //다른 문자열이라면
-                else{
-                    compressedString+=(cnt>2)?cnt+prev:prev;
-                    temp="";
-                    for(int j=i;j<i+charCnt;j++){
-                        if(j<s.length()) temp+=s.charAt(j);
-                    }
-                    cnt=1;//초기화
-                    prev =temp;//초기화
-                }
-            }
-            compressedString+=(cnt>2)?cnt+prev:prev;
-            answer = Math.min(answer,compressedString.length());
+        for(int i=1; i<=(s.length()/2)+1; i++){
+            int result = getSplitedLength(s, i, 1).length();
+            answer = i==1 ? result : (answer>result?result:answer);
         }
+
         return answer;
+    }
+
+    public String getSplitedLength(String s, int n, int repeat){
+        if(s.length() < n) return s;
+        String result = "";
+        String preString = s.substring(0, n);
+        String postString = s.substring(n, s.length());
+
+        // 불일치 -> 현재까지 [반복횟수 + 반복문자] 조합
+        if(!postString.startsWith(preString)){
+            if(repeat ==1) return result += preString + getSplitedLength(postString, n, 1);
+            return result += Integer.toString(repeat) + preString + getSplitedLength(postString, n, 1);
+        }
+
+        return result += getSplitedLength(postString, n, repeat+1);
     }
 }
